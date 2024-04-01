41c4c4555c30934cdfa9834fb54206d445233661
8-prime <hoffmeister.stefan99@gmail.com>
2024-04-01 11:48:45 +0200

    Add docker files for .Net api and change model to fix warning

---
+++ Api/buildandpush.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define the Docker build and push commands
+docker_build_command="docker build -t 8prime/markblogapi ."
+docker_push_command="docker push 8prime/markblogapi"
+
+# Execute the Docker build command
+echo "Running Docker build..."
+eval $docker_build_command
+
+# Check the exit status of the Docker build command
+if [ $? -eq 0 ]; then
+    echo "Docker build completed successfully."
+    
+    # Execute the Docker push command
+    echo "Running Docker push..."
+    eval $docker_push_command
+
+    # Check the exit status of the Docker push command
+    if [ $? -eq 0 ]; then
+        echo "Docker push completed successfully."
+    else
+        echo "Docker push failed."
+    fi
+else
+    echo "Docker build failed."
+fi
