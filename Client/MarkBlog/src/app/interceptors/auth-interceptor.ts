b1debb574588a00612e24307d9327004c251bb64
8-prime <hoffmeister.stefan99@gmail.com>
2024-04-01 11:37:39 +0200

    Restructure interceptor

--- Client/MarkBlog/src/app/interceptors/auth-interceptor.ts
+++ Client/MarkBlog/src/app/interceptors/auth-interceptor.ts
@@ -16,20 +16,15 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept( request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Get the JWT token from local storage
     const token = localStorage.getItem('jwt');
-
+    if(!token) return next.handle(request);
     // Clone the request and add the Authorization header with the JWT token
-    if (token) {
-      const clonedRequest = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+    const clonedRequest = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
       // Pass the cloned request to the next handler
-      return next.handle(clonedRequest);
-    } else {
-      // If there is no token, simply pass the original request
-      return next.handle(request);
-    }
+    return next.handle(clonedRequest);
   }
 }
