511f5f04f6c39f4722d64959bf999e8b1fa02edb
8-prime <hoffmeister.stefan99@gmail.com>
2024-07-17 22:16:30 +0200

    Conceptially working. Razor is throwing hands

--- Markblog.Infrastructure/Backgroundservices/FileWatcherService.cs
+++ Markblog.Infrastructure/Backgroundservices/FileWatcherService.cs
@@ -11,23 +11,24 @@ namespace Markblog.Infrastructure.Backgroundservices
     public class FileWatcherService : BackgroundService
     {
         private const string FileKey = "ArticleFiles";
+
         private FileSystemWatcher _watcher;
         private readonly IServiceProvider _serviceProvider;
-        private readonly Channel<UpdateModel> updateChannel;
-
+        private readonly Channel<UpdateModel> _updateChannel;
+        private readonly string _articleLocation;
 
         public FileWatcherService(IConfiguration config, IServiceProvider provider)
         {
-            updateChannel = Channel.CreateUnbounded<UpdateModel>();
+            _updateChannel = Channel.CreateUnbounded<UpdateModel>();
 
             _serviceProvider = provider;
 
-            var fileLocation = config.GetRequiredSection(FileKey).Value
+            _articleLocation = config.GetRequiredSection(FileKey).Value
                 ?? throw new ArgumentException($"Config key {FileKey} not found. Please specify a valid directory where articles are stored");
 
-            if (!Directory.Exists(fileLocation)) throw new ArgumentException($"{FileKey} is not a valid dictionary");
+            if (!Directory.Exists(_articleLocation)) throw new ArgumentException($"{FileKey} is not a valid dictionary");
 
-            _watcher = new FileSystemWatcher(fileLocation);
+            _watcher = new FileSystemWatcher(_articleLocation);
             _watcher.Created += OnCreated;
             _watcher.Deleted += OnDeleted;
             _watcher.Changed += OnChanged;
@@ -40,15 +41,17 @@ namespace Markblog.Infrastructure.Backgroundservices
             using (var scope = _serviceProvider.CreateScope())
             {
                 var logger = scope.ServiceProvider.GetService<ILogger<FileWatcherService>>();
+                var updateHandler = scope.ServiceProvider.GetRequiredService<UpdateHandlerService>();
                 logger?.LogInformation("Starting watcher service");
+                await updateHandler.CheckDirectory(_articleLocation);
             }
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                await updateChannel.Reader.WaitToReadAsync(stoppingToken);
+                await _updateChannel.Reader.WaitToReadAsync(stoppingToken);
                 using (var scope = _serviceProvider.CreateScope())
                 {
-                    var update = await updateChannel.Reader.ReadAsync(stoppingToken);
+                    var update = await _updateChannel.Reader.ReadAsync(stoppingToken);
                     await Console.Out.WriteLineAsync("Writing changes");
                     var handler = scope.ServiceProvider.GetRequiredService<UpdateHandlerService>();
                     await handler.HandleChanges(update);
@@ -58,17 +61,17 @@ namespace Markblog.Infrastructure.Backgroundservices
 
         private async void OnChanged(object sender, FileSystemEventArgs e)
         {
-            await updateChannel.Writer.WriteAsync(new UpdateModel(Enums.ChangeType.Update, e));
+            await _updateChannel.Writer.WriteAsync(new UpdateModel(Enums.ChangeType.Update, e));
         }
 
         private async void OnCreated(object sender, FileSystemEventArgs e)
         {
-            await updateChannel.Writer.WriteAsync(new UpdateModel(Enums.ChangeType.Creation, e));
+            await _updateChannel.Writer.WriteAsync(new UpdateModel(Enums.ChangeType.Creation, e));
         }
 
         private async void OnDeleted(object sender, FileSystemEventArgs e)
         {
-            await updateChannel.Writer.WriteAsync(new UpdateModel(Enums.ChangeType.Deletion, e));
+            await _updateChannel.Writer.WriteAsync(new UpdateModel(Enums.ChangeType.Deletion, e));
         }
     }
 }
--- Markblog.Infrastructure/Services/UpdateHandlerService.cs
+++ Markblog.Infrastructure/Services/UpdateHandlerService.cs
@@ -1,25 +1,36 @@
-﻿using Markblog.Infrastructure.Backgroundservices;
-using Markblog.Infrastructure.Contexts;
+﻿using Markblog.Infrastructure.Contexts;
 using Markblog.Infrastructure.Enums;
 using Markblog.Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Text;
 
 namespace Markblog.Infrastructure.Services
 {
-    public class UpdateHandlerService(IServiceProvider serviceProvider)
+    public class UpdateHandlerService(ArticleContext context, ILogger<UpdateHandlerService> logger)
     {
         private const int WordsPerMinute = 183;
 
+
+        public async Task CheckDirectory(string directory)
+        {
+            if (!Directory.Exists(directory)) return;
+            var files = Directory.GetFiles(directory);
+            foreach (var file in files)
+            {
+                await ComputeChanges(ChangeType.Creation, file);
+            }
+        }
+
         public async Task HandleChanges(UpdateModel update)
         {
-            using var scope = serviceProvider.CreateScope();
-            using var context = scope.ServiceProvider.GetRequiredService<ArticleContext>();
-            var logger = scope.ServiceProvider.GetRequiredService<ILogger<FileWatcherService>>();
+            await ComputeChanges(update.type, update.e.FullPath);
+        }
 
-            var lines = await File.ReadAllLinesAsync(update.e.FullPath);
+        private async Task ComputeChanges(ChangeType type, string filePath)
+        {
+            logger.LogInformation("Parsing new article {filePath}", filePath);
+            var lines = await File.ReadAllLinesAsync(filePath);
 
             if (lines.Length < 1) return;
 
@@ -27,7 +38,7 @@ namespace Markblog.Infrastructure.Services
 
             if (!lines[0].StartsWith('#')) return;
 
-            title = lines[0][1..^1];
+            title = lines[0][1..^0];
 
             StringBuilder descriptionBuilder = new StringBuilder();
 
@@ -39,12 +50,12 @@ namespace Markblog.Infrastructure.Services
 
             char[] delimiters = [' ', '\r', '\n'];
 
-            for (int i = 0; i < lines.Length; i++)
+            for (int i = 1; i < lines.Length; i++)
             {
                 currentLine = lines[i];
                 if (buildDescription && (currentLine.StartsWith("//")))
                 {
-                    descriptionBuilder.Append(currentLine[1..^1]);
+                    descriptionBuilder.Append(currentLine[2..^0]);
                 }
                 else
                 {
@@ -55,45 +66,28 @@ namespace Markblog.Infrastructure.Services
 
             int readTimeSeconds = totalWords / WordsPerMinute;
 
-
-
-            switch (update.type)
+            switch (type)
             {
                 case ChangeType.Creation:
-                    logger.LogInformation("Update with mode {creation} for {event}", update.type, update.e);
-                    await context.Articles.AddAsync(new Entities.ArticleEntity
-                    {
-                        FilePath = update.e.FullPath,
-                        Title = title,
-                        CreatedDate = DateTime.UtcNow,
-                        Id = Guid.NewGuid(),
-                        ReadDurationSeconds = readTimeSeconds,
-                        UpdatedDate = DateTime.UtcNow,
-                        Description = descriptionBuilder.ToString()
-
-                    });
-                    await context.SaveChangesAsync();
-                    break;
                 case ChangeType.Update:
-                    var existing = await context.Articles.AsNoTracking().FirstOrDefaultAsync(a => a.FilePath == update.e.FullPath);
+                    var existing = await context.Articles.AsNoTracking().FirstOrDefaultAsync(a => a.FilePath == filePath);
                     bool isnew = existing is null;
 
                     existing = new Entities.ArticleEntity
                     {
-                        FilePath = update.e.FullPath,
-                        Title = title,
-                        CreatedDate = existing?.CreatedDate ?? DateTime.UtcNow,
                         Id = existing?.Id ?? Guid.NewGuid(),
-                        ReadDurationSeconds = readTimeSeconds,
+                        CreatedDate = existing?.CreatedDate ?? DateTime.UtcNow,
                         UpdatedDate = DateTime.UtcNow,
-                        Description = descriptionBuilder.ToString()
-
+                        FilePath = filePath,
+                        Title = title,
+                        Description = descriptionBuilder.ToString(),
+                        ReadDurationSeconds = readTimeSeconds,
                     };
                     if (isnew) context.Articles.Add(existing);
                     await context.SaveChangesAsync();
                     break;
                 case ChangeType.Deletion:
-                    await context.Articles.Where(a => a.FilePath.Equals(update.e.FullPath)).ExecuteDeleteAsync();
+                    await context.Articles.Where(a => a.FilePath.Equals(filePath)).ExecuteDeleteAsync();
                     break;
             }
         }
