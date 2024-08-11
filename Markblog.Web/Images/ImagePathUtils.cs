e8055ecf67a42e2b4dce6aedcc15dec66d313a37
8-prime <hoffmeister.stefan99@gmail.com>
2024-08-11 22:20:38 +0200

    Fix image loading

---
+++ Markblog.Web/Images/ImagePathUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace Markblog.Web.Images
+{
+    public class ImagePathUtils
+    {
+        public static string GetImageUrl(string input)
+        {
+            if (string.IsNullOrEmpty(input) || input.StartsWith("//") || input.StartsWith("http"))
+            {
+                return input;
+            }
+            if (input.StartsWith("./"))
+            {
+                input = input[2..^1];
+            }
+            string newSource = $"/api/images/{input}";
+            return newSource;
+        }
+    }
+}
