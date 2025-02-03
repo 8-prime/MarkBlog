f0814a1674dc844efb86d0b06faa119b2603dc2a
8-prime <hoffmeister.stefan99@gmail.com>
2025-02-03 18:29:10 +0100

    Restructure web files

--- backend/Markblog.Web/Endpoints/BlogEndpoints.cs
+++ backend/Markblog.Web/Endpoints/BlogEndpoints.cs
@@ -1,40 +1,28 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using BlazorTemplater;
+using Domain.Entities;
 using Markblog.Application.Interfaces;
 using Markblog.Web.Pages.Components;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Markblog.Application.Mappings;
+using Index = Markblog.Web.Pages.Home.Index;
 
 namespace Markblog.Web.Endpoints;
 
 public static class BlogEndpoints
 {
+    private const int PageSize = 10;
+
     public static WebApplication MapBlogEndpoints(this WebApplication app)
     {
         var group = app.MapGroup("");
         group.MapGet("/", GetHomePage);
-        group.MapGet("/article/{id}", GetArticle);
+        group.MapGet("/article/{id:guid}", GetArticle);
+        group.MapGet("/article-overview/", GetArticleOverview);
         return app;
-    }
-
-    private static async Task<IResult> GetHomePage(
-        [FromServices] IBlogDbContext dbContext)
-    {
-        var articles = await dbContext.Articles.ToListAsync();
-        var landingHtml = new ComponentRenderer<ArticlesOverview>()
-            .Set(c => c.Articles, articles)
-            .Render();
-        if (landingHtml is null)
-        {
-            return TypedResults.InternalServerError();
-        }
-
-        return Results.Text(content: landingHtml,
-            contentType: "text/html",
-            statusCode: (int)HttpStatusCode.OK);
     }
 
     private static async Task<IResult> GetArticle(Guid id,
@@ -54,5 +42,47 @@ public static class BlogEndpoints
         }
 
         return Results.Text(content: articleHtml, contentType: "text/html", statusCode: (int)HttpStatusCode.OK);
+    }
+
+    private static async Task<IResult> GetHomePage(
+        [FromServices] IBlogDbContext dbContext)
+    {
+        var articles = await GetArticlesForPage(0, dbContext);
+        var landingHtml = new ComponentRenderer<Index>()
+            .Set(c => c.Articles, articles)
+            .Set(c => c.PageSize, PageSize)
+            .Render();
+        if (landingHtml is null)
+        {
+            return TypedResults.InternalServerError();
+        }
+
+        return Results.Text(content: landingHtml,
+            contentType: "text/html",
+            statusCode: (int)HttpStatusCode.OK);
+    }
+
+    private static async Task<IResult> GetArticleOverview(int page, [FromServices] IBlogDbContext dbContext)
+    {
+        var articles = await GetArticlesForPage(page, dbContext);
+
+        var landingHtml = new ComponentRenderer<ArticlesOverview>()
+            .Set(c => c.Articles, articles)
+            .Set(c => c.Page, page)
+            .Set(c => c.PageSize, PageSize)
+            .Render();
+        return Results.Text(content: landingHtml,
+            contentType: "text/html",
+            statusCode: (int)HttpStatusCode.OK);
+    }
+
+    private static async Task<List<ArticleEntity>> GetArticlesForPage(int page, IBlogDbContext dbContext)
+    {
+        return await dbContext.Articles
+            .AsNoTracking()
+            .OrderByDescending(a => a.CreatedDate)
+            .Skip(page * PageSize)
+            .Take(PageSize)
+            .ToListAsync();
     }
 }
