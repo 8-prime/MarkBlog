11b8bc43568c99ae7d7a0432cd540134e4375fa5
8-prime <hoffmeister.stefan99@gmail.com>
2025-10-04 18:47:15 +0200

    Update unauthorized view to center text

--- frontend/src/pages/Unauthorized.tsx
+++ frontend/src/pages/Unauthorized.tsx
@@ -1,6 +1,6 @@
 function Unauthorized() {
     return (
-        <div className="w-screen h-screen bg-background">
+        <div className="w-screen h-screen bg-background flex justify-center items-center">
             <h1 className="text-text">You are not authorized to edit articles here</h1>
             <a href="/" className="text-primary">Go back to viewing articles</a>
         </div>
