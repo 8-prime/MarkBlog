cf1a597dacc1e1e5679101ca399e5c2ba2b2c0f2
8-prime <hoffmeister.stefan99@gmail.com>
2025-10-06 22:30:21 +0200

    Minor css changes to admin page

--- frontend/src/components/ArticleEditForm.tsx
+++ frontend/src/components/ArticleEditForm.tsx
@@ -163,7 +163,7 @@ const ArticleEditForm: React.FC<Props> = ({ formData, setFormData, onCancel, onS
                 </div>
 
                 {/* Right: Editor */}
-                <div className="flex-1 p-6 overflow-y-auto">
+                <div className="flex-1 overflow-y-auto">
                     <Editor
                         articleText={formData.body}
                         setArticleText={(text) => handleInputChange("body", text ?? "")}
